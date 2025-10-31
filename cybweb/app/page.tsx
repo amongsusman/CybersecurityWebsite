ebcae17d523e2a784698abc3e9776713bb152509
amongsusman <149024306+amongsusman@users.noreply.github.com>
2025-10-31 17:31:53 +0000

    made all routes and home

---
+++ cybweb/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-col min-h-screen bg-zinc-50 font-sans dark:bg-black" style={{border: '1px solid red'}}>
+      <div className="h-3/4 bg-gradient-to-r from-blue-300 to-blue-600" style={{border: '1px solid red'}}>
+        Daojiw
+      </div>
+    </div>
+  );
+}
